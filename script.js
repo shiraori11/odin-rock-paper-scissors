b5f548d698075b9f2af3ed6ec735a628627b31a0
shiraori11 <senpaiyui69@gmail.com>
2026-05-18 07:38:51 +0800

    Added a function so players can play a round against computer

--- script.js
+++ script.js
@@ -9,17 +9,45 @@ function showChoiceAsString(choice) {
   switch(choice) {
     case 1:
       return "rock";
+      break;
     case 2:
       return "paper";
+      break;
     case 3:
       return "scissor";
+      break;
   }
 }
 
 function getHumanChoice() {
-  const humanInput = prompt("Pick a choice '1' for Rock, '2' for Paper, '3' for Scissor \nEnter a digit: ");
+  const humanInput = prompt("Rock, Paper, or Scissors? ");
   return humanInput;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(player, computer) {
+  let playerToLower = player.toString().toLowerCase();
+
+  if (playerToLower === computer) {
+    console.log("Tie!");
+    return;
+  } else if (playerToLower === "rock" && computer === "scissor") {
+    console.log("You Win! Rock beats Scissor!");
+    return;
+  } else if (playerToLower === "paper" && computer === "rock") {
+    console.log("You Win! Paper beats Rock!");
+    return;
+  } else if (playerToLower === "scissor" && computer === "paper") {
+    console.log("You Win! Scissor beats Paper!");
+    return;
+  } else {
+    console.log(`You Loss! ${capitalize(computer)} beats ${capitalize(playerToLower)}`);
+    return;
+  }
+}
+
+function capitalize(text) {
+  const convertTextToString = text.toString();
+  return convertTextToString.charAt(0).toUpperCase() + convertTextToString.slice(1);
+}
+
+playRound(getHumanChoice(), getComputerChoice());
