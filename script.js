253004cab78ea0feec97ec4d3a4a62fc0710baac
shiraori11 <senpaiyui69@gmail.com>
2026-05-17 21:02:10 +0800

    Added random choice logic for the computer opponent

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+  const randomChoice = Math.floor(Math.random() * 3) + 1;
+  return randomChoice;
+}
+
+console.log(getComputerChoice());
